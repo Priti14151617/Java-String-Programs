002372237d6863bf71983e87bd59c52f319c4f11
priti kumari <151085priti@gmai;.com>
2024-03-04 14:40:18 +0800

    count vowels

---
+++ src/StringProgram.java
@@ -0,0 +1,20 @@
+public class StringProgram {
+
+    static int countVowels(String s){
+        int count =0;
+        for(int i=0; i<s.length(); i++){
+            char c= s.charAt(i);
+            if(c =='a'|| c =='e'||  c =='i'|| c =='o'|| c =='u'|| c =='A'||
+                c =='E'|| c =='I'|| c =='O'|| c =='U'){
+                count++;
+            }
+
+        }
+        return count;
+
+    }
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println("No. of Vowels in Strings: " +countVowels(s));
+    }
+}
